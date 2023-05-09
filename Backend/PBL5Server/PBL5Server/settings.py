51f3b438b10e6bc0abb0ba3e05b2ddad190745b9
minhkycho18 <80151408+minhkycho18@users.noreply.github.com>
2023-05-09 21:33:58 +0700

    comment tensor

--- Backend/PBL5Server/PBL5Server/settings.py
+++ Backend/PBL5Server/PBL5Server/settings.py
@@ -140,10 +140,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['*']
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
 CORS_ALLOW_METHODS = (
     "DELETE",
     "GET",
